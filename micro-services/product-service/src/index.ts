0c54571905a53d556e54ca660d77863bc8241428
gorbayotte <mochonlucas@gmail.com>
2026-02-09 09:16:17 +0100

    config service product service

--- micro-services/product-service/src/index.ts
+++ micro-services/product-service/src/index.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
+import { Logger } from './common/logger'
+import { initDb } from './models';
 
 dotenv.config();
 const app = express()
@@ -8,8 +10,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+initDb()
+
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000
 
 app.listen(PORT, () => {
-  console.log(`Service connecté sur le port ${PORT}`)
+  Logger.info(`Service connecté sur le port ${PORT}`)
 })
