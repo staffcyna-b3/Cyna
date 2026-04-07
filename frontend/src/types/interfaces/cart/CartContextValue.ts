9cb3d4882018fd1e634dbe8ffdcb51aab88bd0ae
Marc Désir Bouanga Niambi <>
2026-04-07 22:06:10 +0200

    refactor: update cart types

--- frontend/src/types/interfaces/cart/CartContextValue.ts
+++ frontend/src/types/interfaces/cart/CartContextValue.ts
@@ -2,6 +2,7 @@ import { CartItem } from './CartItem';
 import { AddToCartOptions } from './AddToCartOptions';
 
 export interface CartContextValue {
+    cartId: string | null;
     items: CartItem[];
     totalAmount: number;
     isLoading: boolean;
--- frontend/src/types/interfaces/cart/CartItem.ts
+++ frontend/src/types/interfaces/cart/CartItem.ts
@@ -1,8 +1,12 @@
 export interface CartItem {
   id: string;
-  product_id: string;
+  productId: string;
   name: string;
-  price: number;
   quantity: number;
-  is_service: boolean;
+  unitPrice: number;
+  subtotal: number;
+  isService: boolean;
+  unavailable?: boolean;
+  imageUrl?: string;
+  billingPeriod?: string;
 }
