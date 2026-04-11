ddd91c858aecfd0134791cc02aae260ee68d0202
blnche <peltierblanche@gmail.com>
2026-04-11 16:17:01 +0200

    WIP

--- micro-services/front-office-service/src/common/httpError.ts
+++ micro-services/front-office-service/src/common/httpError.ts
@@ -1,8 +1,7 @@
 export class HttpError extends Error {
-  statusCode: number;
-
-  constructor(statusCode: number, message: string) {
+  constructor(public statusCode: number, message: string) {
     super(message);
-    this.statusCode = statusCode;
+    this.name = 'HttpError';
+    Object.setPrototypeOf(this, HttpError.prototype);
   }
 }
